27708027daad973f2d0e859e08e930d6a15f2e1f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-03-26 11:39:26 +0530

    Setup initial dev environment

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const path = require("path");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
