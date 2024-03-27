a0ed6fbe09078172ef9e2e4dbe74d79c53f05e70
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-03-27 11:13:44 +0530

    Add errorHandler to app's middleware chain

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const path = require("path");
+const errorHandler = require("./middlewares/errorHandler");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -17,6 +18,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
