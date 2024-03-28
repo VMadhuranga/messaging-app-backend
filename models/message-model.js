ddfa000f5bb45115e416476948953d73b836c160
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-03-28 15:42:06 +0530

    Create initial message model

---
+++ models/message-model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const MessageSchema = new Schema({
+  participants: [{ type: Schema.Types.ObjectId }],
+  messages: [
+    {
+      senderID: { type: Schema.Types.ObjectId, required: true },
+      content: { type: String, required: true },
+      date: { type: Date, default: Date.now() },
+    },
+  ],
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
