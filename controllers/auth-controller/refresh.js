b4a419d8278f847aaf03535dc33cec0db17909ab
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-26 14:32:16 +0530

    Create refresh middleware

---
+++ controllers/auth-controller/refresh.js
@@ -0,0 +1,44 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+const UserModel = require("../../models/user-model");
+
+const refresh = async (req, res) => {
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+
+      const user = await UserModel.findOne({ userName: decoded.username })
+        .lean()
+        .exec();
+
+      if (!user) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      const accessToken = jwt.sign(
+        {
+          username: user.userName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m" },
+      );
+
+      res.json({ accessToken });
+    }),
+  );
+};
+
+module.exports = refresh;
