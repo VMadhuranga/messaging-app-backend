892f9540966b7f2c5f7f593c12073e2ecfaf44a6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-26 14:37:14 +0530

    Remove unnecessary files

--- routes/login-route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const loginController = require("../controllers/auth-controller/login");
-
-const router = express.Router();
-
-router.post("/login", loginController);
-
-module.exports = router;
