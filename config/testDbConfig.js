ca9940bc5108a922e0dc449a6577030bfd9acea5
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-26 14:26:13 +0530

    Update test database config

--- config/testDbConfig.js
+++ config/testDbConfig.js
@@ -1,23 +1,6 @@
-const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
-
-async function initializeTestMongoDBServer() {
-  const mongoDBServer = await MongoMemoryServer.create();
-  const mongoUri = mongoDBServer.getUri();
-
-  mongoose.connect(mongoUri);
-
-  mongoose.connection.on("error", (e) => {
-    if (e.message.code === "ETIMEDOUT") {
-      console.log(e);
-      mongoose.connect(mongoUri);
-    }
-    console.log(e);
-  });
-
-  mongoose.connection.once("open", () => {
-    console.log(`MongoDB successfully connected to ${mongoUri}`);
-  });
-}
-
-module.exports = initializeTestMongoDBServer;
+module.exports = {
+  Memory: true,
+  IP: "127.0.0.1",
+  Port: "27017",
+  Database: "testDB",
+};
