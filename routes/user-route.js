4d850023a54177d45408d112cec317b6d8760834
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-29 13:43:58 +0530

    Create router to handle DELETE request in /users/:user_id route

--- routes/user-route.js
+++ routes/user-route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/users", userController.createUser);
 router.get("/users/:user_id", userController.getUser);
+router.delete("/users/:user_id", userController.deleteUser);
 
 module.exports = router;
