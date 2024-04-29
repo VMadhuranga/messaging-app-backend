ea326ab782bd7545fc713bcf982b3671779683ba
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-29 12:05:50 +0530

    Create router to handle GET request in /users/:user_id route

--- routes/user-route.js
+++ routes/user-route.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/user-controller/");
 const router = express.Router();
 
 router.post("/users", userController.createUser);
+router.get("/users/:user_id", userController.getUser);
 
 module.exports = router;
