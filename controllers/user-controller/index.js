25a7026dccac3cec028cba143f9a04fcdfe1397f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-29 13:43:37 +0530

    Add deleteUser middleware to userController index

--- controllers/user-controller/index.js
+++ controllers/user-controller/index.js
@@ -1,7 +1,9 @@
 const createUser = require("./create-user");
 const getUser = require("./get-user");
+const deleteUser = require("./delete-user");
 
 module.exports = {
   createUser,
   getUser,
+  deleteUser,
 };
