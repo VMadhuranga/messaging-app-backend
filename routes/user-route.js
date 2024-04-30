9f16e512ae24ed15635ada4de94668ab535c6e71
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-30 09:48:58 +0530

    Create router to handle PATCH request in /:user_id/first_name route

--- routes/user-route.js
+++ routes/user-route.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/users", userController.createUser);
 router.get("/users/:user_id", userController.getUser);
 router.delete("/users/:user_id", userController.deleteUser);
+router.patch("/users/:user_id/first_name", userController.updateUserFirstName);
 
 module.exports = router;
