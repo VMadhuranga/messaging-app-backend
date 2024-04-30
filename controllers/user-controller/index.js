97926f6833971de81e139f7dff5a313596ce515c
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-04-30 09:48:38 +0530

    Add updateUserFirstName middleware to userController index

--- controllers/user-controller/index.js
+++ controllers/user-controller/index.js
@@ -1,9 +1,11 @@
 const createUser = require("./create-user");
 const getUser = require("./get-user");
 const deleteUser = require("./delete-user");
+const updateUserFirstName = require("./update-user-first-name");
 
 module.exports = {
   createUser,
   getUser,
   deleteUser,
+  updateUserFirstName,
 };
