356c055ad846f2b145d287fb1cdbb6a283cf8f65
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-01 09:01:56 +0530

    Create router to handle PATCH request in /:user_id/username route

--- routes/user-route.js
+++ routes/user-route.js
@@ -9,5 +9,6 @@ router.get("/users/:user_id", userController.getUser);
 router.delete("/users/:user_id", userController.deleteUser);
 router.patch("/users/:user_id/first_name", userController.updateUserFirstName);
 router.patch("/users/:user_id/last_name", userController.updateUserLastName);
+router.patch("/users/:user_id/username", userController.updateUserUsername);
 
 module.exports = router;
