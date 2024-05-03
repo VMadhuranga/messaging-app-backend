7e62dea516d21103b9ca8b9c1e0fa0d3f44759c1
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-03 13:31:10 +0530

    Create router to handle GET request in /:user_id/friends route

--- routes/friend-route.js
+++ routes/friend-route.js
@@ -5,5 +5,6 @@ const friendController = require("../controllers/friend-controller/");
 const router = express.Router();
 
 router.post("/:user_id/friends", friendController.addFriend);
+router.get("/:user_id/friends", friendController.getFriends);
 
 module.exports = router;
