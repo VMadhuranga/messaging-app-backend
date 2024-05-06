604415270a64e732b6278325a676dea7d84d6b94
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-06 11:01:32 +0530

    Update message model

--- models/message-model.js
+++ models/message-model.js
@@ -2,14 +2,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const MessageSchema = new Schema({
-  participants: [{ type: Schema.Types.ObjectId }],
-  messages: [
-    {
-      senderID: { type: Schema.Types.ObjectId, required: true },
-      content: { type: String, required: true },
-      date: { type: Date, default: Date.now() },
-    },
-  ],
+  senderID: { type: Schema.Types.ObjectId, required: true },
+  receiverID: { type: Schema.Types.ObjectId, required: true },
+  content: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
