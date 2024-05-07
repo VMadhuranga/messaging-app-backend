38d3cc37faa9ea44fa71e559f09936ffc01f7dd6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-07 14:27:12 +0530

    Update deleteUser middleware to delete user friends, sent messages and received messages

--- controllers/user-controller/delete-user.js
+++ controllers/user-controller/delete-user.js
@@ -2,6 +2,8 @@ const asyncHandler = require("express-async-handler");
 const { param, validationResult } = require("express-validator");
 
 const UserModel = require("../../models/user-model");
+const FriendModel = require("../../models/friend-model");
+const MessageModel = require("../../models/message-model");
 
 const deleteUser = [
   param("user_id").trim().isMongoId().escape(),
@@ -12,6 +14,18 @@ const deleteUser = [
     if (!errors.isEmpty()) {
       return res.status(404).json({ message: "Resource not found" });
     }
+
+    await FriendModel.deleteMany({
+      userID: req.params.user_id,
+    }).exec();
+
+    await MessageModel.deleteMany({
+      senderID: req.params.user_id,
+    }).exec();
+
+    await MessageModel.deleteMany({
+      receiverID: req.params.user_id,
+    }).exec();
 
     await UserModel.findByIdAndDelete(req.params.user_id).exec();
 
