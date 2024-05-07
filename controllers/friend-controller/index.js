39eb86f7b20ae3497714afc21cd6602c6b0e1821
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-07 09:09:22 +0530

    Add deleteFriend middleware to friendController index

--- controllers/friend-controller/index.js
+++ controllers/friend-controller/index.js
@@ -1,7 +1,9 @@
 const addFriend = require("./add-friend");
 const getFriends = require("./get-friends");
+const deleteFriend = require("./delete-friend");
 
 module.exports = {
   addFriend,
   getFriends,
+  deleteFriend,
 };
