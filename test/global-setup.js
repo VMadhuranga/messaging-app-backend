5c6ac51a34cbfd9a54f726c3e4565b31a10af6fd
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-09 09:26:26 +0530

    Update globalSetup function

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,23 +1,15 @@
 const { MongoMemoryServer } = require("mongodb-memory-server");
-const mongoose = require("mongoose");
 const config = require("../config/testDbConfig");
 
 async function globalSetup() {
   if (config.Memory) {
     const instance = await MongoMemoryServer.create();
     const uri = instance.getUri();
-    console.log(uri);
     globalThis.__MONGOINSTANCE = instance;
     process.env.MONGO_URI = uri.slice(0, uri.lastIndexOf("/"));
   } else {
     process.env.MONGO_URI = `mongodb://${config.IP}:${config.Port}`;
   }
-
-  const conn = await mongoose.connect(
-    `${process.env.MONGO_URI}/${config.Database}`,
-  );
-  await conn.connection.db.dropDatabase();
-  await mongoose.disconnect();
 }
 
 module.exports = globalSetup;
