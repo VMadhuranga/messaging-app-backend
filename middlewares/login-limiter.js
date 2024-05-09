579a624ce6d82ce7a6a1c1d7c01ef4537bc61d28
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-09 11:39:44 +0530

    Create loginLimiter middleware

---
+++ middlewares/login-limiter.js
@@ -0,0 +1,14 @@
+const rateLimit = require("express-rate-limit");
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: {
+    message:
+      "Too many login attempts, please try again after a 60 second pause",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = loginLimiter;
