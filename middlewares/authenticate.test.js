d3455720e43c0b78f0dca04e531f19a84606f84f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-26 13:54:33 +0530

    Update test cases for authenticate middleware

--- middlewares/authenticate.test.js
+++ middlewares/authenticate.test.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const request = require("supertest");
-const bcrypt = require("bcryptjs");
 
 const UserModel = require("../models/user-model");
 const authRouter = require("../routes/auth-route");
@@ -15,32 +14,18 @@ app.use("/", authRouter);
 app.use("/", userRouter);
 
 describe("GET /users/:user_id", () => {
-  const testUser = {
-    first_name: "john",
-    last_name: "doe",
-    username: "jd",
-    password: "jd1234",
-  };
-
   it("should grant access to protected routes if authenticated", async () => {
-    const newUser = new UserModel({
-      firstName: testUser.first_name,
-      lastName: testUser.last_name,
-      userName: testUser.username,
-      password: await bcrypt.hash(testUser.password, 10),
-    });
-
-    const { id } = await newUser.save();
-
     const loginResponse = await request(app).post("/login").send({
-      username: testUser.username,
-      password: testUser.password,
+      username: "jd",
+      password: "jd1234",
     });
 
     expect(loginResponse.statusCode).toBe(200);
 
+    const user = await UserModel.findOne({ userName: "jd" }).lean().exec();
+
     const getUserResponse = await request(app)
-      .get(`/users/${id}`)
+      .get(`/users/${user._id}`)
       .set("Authorization", `Bearer ${loginResponse.body.accessToken}`);
 
     expect(getUserResponse.statusCode).toBe(200);
