1c4b49cd7234bf637ddd094ed961b7aba0e6e712
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-26 13:54:17 +0530

    Update test cases for loginLimiter middleware

--- middlewares/login-limiter.test.js
+++ middlewares/login-limiter.test.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const request = require("supertest");
-const bcrypt = require("bcryptjs");
 
-const UserModel = require("../models/user-model");
 const authRouter = require("../routes/auth-route");
 
 const app = express();
@@ -13,32 +11,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", authRouter);
 
 describe("POST /login", () => {
-  const testUser = {
-    first_name: "john",
-    last_name: "doe",
-    username: "jd",
-    password: "jd1234",
-  };
-
-  beforeEach(async () => {
-    const newUser = new UserModel({
-      firstName: testUser.first_name,
-      lastName: testUser.last_name,
-      userName: testUser.username,
-      password: await bcrypt.hash(testUser.password, 10),
-    });
-
-    await newUser.save();
-  });
-
   it("should give error message if rate limit exceeds", async () => {
     let loginResponse;
 
     // simulate rate limit
     for (let i = 0; i < 6; i++) {
       loginResponse = await request(app).post("/login").send({
-        username: testUser.username,
-        password: testUser.password,
+        username: "jd",
+        password: "jd1234",
       });
     }
 
